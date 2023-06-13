51bef801412b29459929284a6d385a4ce031df4a
Liam <liamthorne4@gmail.com>
2023-06-13 17:09:42 +0100

    Not working currently

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,37 @@
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from pytorch_lightning import LightningModule
+import os
+from datasets import load_from_disk, load_dataset
 
+class TextDataset(object):
+    def __init__(self, tokenizer, label_tokenizer, max_length=512):
+        super(TextDataset, self).__init__()
 
-class 
+        self.max_length = max_length
+        self.tokenizer = tokenizer
+        self.label_tokenizer = label_tokenizer
+
+    def tokenize(self, text):
+        result = self.tokenizer(
+            text,
+            truncation=True,
+            max_length=self.max_length,
+            padding=False,
+        )
+
+        result['labels'] = self.label_tokenizer.encode(text)
+
+        return result
+
+    def load_data(self, data_path):
+        if data_path.endswith('.json') or data_path.endswith('.jsonl'):
+            data = load_dataset('json', data_files=data_path)
+        elif os.path.isdir(data_path):
+            data = load_from_disk(data_path)
+        else:
+            load_dataset(data_path)
+
+        print(data['train'])
+        data['train'] = data['train'].map(lambda x: self.tokenize(x['text']), remove_columns=['text'])
+        print(data['train'])
+        return data['train']
--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from torch.optim.lr_scheduler import LambdaLR
 from pytorch_lightning import LightningModule, seed_everything, Trainer
 from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader
-from datasets import load_from_disk
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCasualLM
+from datasets import load_from_disk, load_dataset
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM, AutoModel
+from transformers import DataCollatorForSeq2Seq
+import os
+import torch
+from data_loader import TextDataset
 
 class CycleModel(LightningModule):
     def __init__(self, model_a_name_or_path, task_a, data_a, data_b, model_b_name_or_path=None, task_b=None, batch_size=32):
@@ -18,28 +22,38 @@ class CycleModel(LightningModule):
             model_b_name_or_path = model_a_name_or_path
         
         if task_a.upper() == 'CAUSAL_LM':
-            self.model_a = AutoModelForCasualLM.from_pretrained(model_a_name_or_path)
+            self.model_a = AutoModelForCausalLM.from_pretrained(model_a_name_or_path)
         elif task_a.upper() == 'SEQ2SEQ_LM':
-            self.model_a = AutoModelForSeq2SeqLM.from_pretrained(model_a_name_or_path)
+            self.model_a = AutoModel.from_pretrained(model_a_name_or_path)
+        else:
+            raise ValueError(f'Unknown task {task_a}')
 
         if task_b.upper() == 'CAUSAL_LM':
-            self.model_b = AutoModelForCasualLM.from_pretrained(model_b_name_or_path)
+            self.model_b = AutoModelForCausalLM.from_pretrained(model_b_name_or_path)
         elif task_b.upper() == 'SEQ2SEQ_LM':
-            self.model_b = AutoModelForSeq2SeqLM.from_pretrained(model_b_name_or_path)
+            self.model_b = AutoModel.from_pretrained(model_b_name_or_path)
+        else:
+            raise ValueError(f'Unknown task {task_b}')
 
         self.tokenizer_a = AutoTokenizer.from_pretrained(model_a_name_or_path)
         self.tokenizer_b = AutoTokenizer.from_pretrained(model_b_name_or_path)
+
+        if not self.tokenizer_a.pad_token:
+            self.tokenizer_a.pad_token = self.tokenizer_a.eos_token
+        if not self.tokenizer_b.pad_token:
+            self.tokenizer_b.pad_token = self.tokenizer_b.eos_token
+
         self.data_a = data_a
         self.data_b = data_b
         self.batch_size = batch_size
 
     def configure_optimizers(self):
         opt_a = Adam(
-            self.model_a.paramerters(),
+            self.model_a.parameters(),
             lr=2e-4, betas=(0.5, 0.999))
         
         opt_b = Adam(
-            self.model_a.paramerters(),
+            self.model_b.parameters(),
             lr=2e-4, betas=(0.5, 0.999))
         
         gamma = lambda epoch: 1 - max(0, epoch + 1 - 100) / 101
@@ -47,18 +61,29 @@ class CycleModel(LightningModule):
         sch_b = LambdaLR(opt_b, lr_lambda=gamma)
         return [opt_a, opt_b], [sch_a, sch_b]
     
-    def _cycle(self, batch, generating_model, training_model, training_tokenizer):
-        input_ids, attention_mask, labels = batch['input_ids'], batch['attention_mask'], batch['labels']
+    def _cycle(self, batch, generating_model, training_model, generating_tokenizer, training_tokenizer):
+        print(batch)
+        input_ids, attention_mask = batch['input_ids'], batch['attention_mask']
         generated_ids = generating_model.generate(
             input_ids=input_ids,
             attention_mask=attention_mask
             )
         
+        # decoded_ids = generating_tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+        # decoded_input_ids = generating_tokenizer.batch_decode(input_ids, skip_special_tokens=True)
+        # training_ids, training_mask = training_tokenizer.batch_encode_plus(decoded_ids, padding=True, return_tensors='pt').values()
+        # labels = training_tokenizer.batch_encode_plus(decoded_input_ids, padding=True, return_tensors='pt')['input_ids']
+
+        # Skip decode-encode by removing leading pad and generating an attention mask
         generated_ids = generated_ids[:, 1:]
+
+        # Create attention mask
         attention_mask = generated_ids.clone()
         attention_mask[attention_mask != 0] = 1
 
-        labels[labels==training_tokenizer.pad_token_id] = -100
+        labels = input_ids.clone()
+        # Set pad tokens to be ignored by PyTorch loss function
+        labels[labels==generating_tokenizer.pad_token_id] = -100
 
         outputs = training_model(
             input_ids      = generated_ids.to(self.device),
@@ -70,15 +95,32 @@ class CycleModel(LightningModule):
     
     def training_step(self, batch, batch_idx, optimizer_idx):
         if optimizer_idx == 0:
-            loss_a = self._cycle(batch, self.model_b, self.model_a, self.tokenizer_a).loss
+            print('A')
+            loss_a = self._cycle(batch['a'], self.model_b, self.model_a, self.tokenizer_b, self.tokenizer_a).loss
             return {'loss': loss_a}
         elif optimizer_idx == 1:
-            loss_b = self._cycle(batch, self.model_a, self.model_b, self.tokenizer_b).loss
+            print('B')
+            loss_b = self._cycle(batch['b'], self.model_a, self.model_b, self.tokenizer_a, self.tokenizer_b).loss
             return {'loss ': loss_b}
+        
+    def _load_dataset(self, dataset):
+        if dataset.endswith('.json') or dataset.endswith('.jsonl'):
+            dataset = load_dataset('json', data_files=dataset)
+        elif os.path.isdir(dataset):
+            dataset = load_from_disk(dataset)
+        else:
+            dataset = load_dataset(dataset)
+        return dataset
 
     def train_dataloader(self):
-        loader_a = DataLoader(load_from_disk(self.data_a), self.batch_size, shuffle=True)
-        loader_b = DataLoader(load_from_disk(self.data_b), self.batch_size, shuffle=True)
+        self.collator_a = DataCollatorForSeq2Seq(self.tokenizer_a, return_tensors='pt', padding=True)
+        self.collator_b = DataCollatorForSeq2Seq(self.tokenizer_b, return_tensors='pt', padding=True)
+
+        dataset_a = TextDataset(self.tokenizer_a, self.tokenizer_b).load_data(self.data_a)
+        dataset_b = TextDataset(self.tokenizer_b, self.tokenizer_a).load_data(self.data_b)
+
+        loader_a = DataLoader(dataset_a, self.batch_size, shuffle=True, collate_fn=self.collator_a)
+        loader_b = DataLoader(dataset_b, self.batch_size, shuffle=True, collate_fn=self.collator_b)
         return {"a": loader_a, "b": loader_b}
 
 def main(args):
@@ -95,22 +137,23 @@ def main(args):
     )
 
     trainer = Trainer.from_argparse_args(args)
-    trainer.logger = WandbLogger(project=args.wandb_project, entity=args.wandb_entity, name=args.wandb_run_name)
+    if args.use_wandb:
+        trainer.logger = WandbLogger(project=args.wandb_project, entity=args.wandb_entity, name=args.wandb_run_name)
     trainer.fit(model)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_a_name_or_path', type=str, default='gpt2')
     parser.add_argument('--model_b_name_or_path', type=str, default='gpt2')
-    parser.add_argument('--data_a', type=str, default='data/a')
-    parser.add_argument('--data_b', type=str, default='data/b')
+    parser.add_argument('--data_a', type=str, default='data/a.json')
+    parser.add_argument('--data_b', type=str, default='data/b.json')
     parser.add_argument('--task_a', type=str, default='causal_lm')
     parser.add_argument('--task_b', type=str, default='causal_lm')
-    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--batch_size', type=int, default=2)
     parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--use_wandb', action='store_true')
+    parser.add_argument('--use_wandb', action='store_true', default=False)
     parser.add_argument('--wandb_project', type=str, default='CycleTraining')
-    parser.add_argument('--wandb_entity', type=str, default='wandb')
+    parser.add_argument('--wandb_entity', type=str, default=None)
     parser.add_argument('--wandb_run_name', type=str, default='CycleTraining')
     args = parser.parse_args()
     main(args)
