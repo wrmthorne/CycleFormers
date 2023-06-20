68ee1e87f2c28f11acc8f8429b542031d0133d7d
Liam <liamthorne4@gmail.com>
2023-06-20 08:44:42 +0100

    Reorganised code and added model saving on completion

--- train.py
+++ train.py
@@ -1,164 +1,7 @@
 import argparse
-from torch.optim import Adam
-from torch.optim.lr_scheduler import LambdaLR
-from pytorch_lightning import LightningModule, seed_everything, Trainer
+from pytorch_lightning import seed_everything, Trainer
 from pytorch_lightning.loggers import WandbLogger
-from torch.utils.data import DataLoader
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM
-from transformers import DataCollatorForSeq2Seq
-import torch
-from data_loader import TextDataset
-from torch.nn.utils.rnn import pad_sequence
-import sys
-from lightning.pytorch.utilities import CombinedLoader
-
-class CycleModel(LightningModule):
-    def __init__(self, model_name_or_path, data_a, data_b, task, batch_size, gradient_accumulation_steps):
-        super().__init__()
-
-        self.task = task.upper()
-
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-
-        if not self.tokenizer.pad_token:
-            self.tokenizer.pad_token = self.tokenizer.eos_token
-        
-        if self.task == 'CAUSAL_LM':
-            self.model_a = AutoModelForCausalLM.from_pretrained(model_name_or_path)
-            self.model_b = AutoModelForCausalLM.from_pretrained(model_name_or_path)
-            self.tokenizer.padding_side = 'left'
-            self.cycle = self._cycle_causal
-        elif self.task == 'SEQ2SEQ_LM':
-            self.model_a = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
-            self.model_b = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
-            self.cycle = self._cycle_seq2seq
-        else:
-            raise ValueError(f'Unknown task {self.task}')
-
-        self.collator_a = DataCollatorForSeq2Seq(self.tokenizer, return_tensors='pt', padding=True)
-        self.collator_b = DataCollatorForSeq2Seq(self.tokenizer, return_tensors='pt', padding=True)
-
-        self.train_data_a, self.val_data_a = TextDataset(self.tokenizer).load_data(data_a)
-        self.train_data_b, self.val_data_b = TextDataset(self.tokenizer).load_data(data_b)
-
-        self.batch_size = batch_size
-        self.gradient_accumulation_steps = gradient_accumulation_steps
-
-        self.automatic_optimization = False
-
-    def configure_optimizers(self):
-        opt_a = Adam(
-            self.model_a.parameters(),
-            lr=2e-4, betas=(0.5, 0.999))
-        
-        opt_b = Adam(
-            self.model_b.parameters(),
-            lr=2e-4, betas=(0.5, 0.999))
-        
-        gamma = lambda epoch: 1 - max(0, epoch + 1 - 100) / 101
-        sch_a = LambdaLR(opt_a, lr_lambda=gamma)
-        sch_b = LambdaLR(opt_b, lr_lambda=gamma)
-        return [opt_a, opt_b], [sch_a, sch_b]
-    
-
-    def _cycle_causal(self, batch, generating_model, training_model):
-        # Input --GenModel.generate>> Input + ResponseA
-        # ResponseA + Input --TrainModel.__call__>> Reconstruction Loss
-        prompt_gen, attention_mask = batch['input_ids'], batch['attention_mask']
-        prompt_response_gen = generating_model.generate(
-            input_ids      = prompt_gen.to(self.device),
-            attention_mask = attention_mask.to(self.device),
-            pad_token_id   = self.tokenizer.pad_token_id,
-            )
-        
-        # Split response from prompt
-        response_gen = prompt_response_gen[:, prompt_gen.shape[-1]:]
-
-        # Switch generator models prompt and response to be new labels
-        labels = torch.cat((response_gen.to(self.device), prompt_gen), dim=1)
-        labels[labels==self.tokenizer.pad_token_id] = -100
-
-        # pad new input to match label size
-        pad_size = labels.shape[-1] - response_gen.shape[-1]
-        pad_tensor = torch.full((labels.shape[0], pad_size), self.tokenizer.pad_token_id)
-        prompt_train = torch.cat((pad_tensor.to(self.device), response_gen), dim=-1)
-        attention_mask_train = torch.ones_like(prompt_train)
-
-        outputs = training_model(
-            input_ids      = prompt_train.to(self.device),
-            attention_mask = attention_mask_train.to(self.device),
-            labels         = labels.to(self.device)
-            )
-        
-        return outputs
-    
-    def _cycle_seq2seq(self, batch, generating_model, training_model):
-        input_gen, attention_mask_gen = batch['input_ids'], batch['attention_mask']
-        response_gen = generating_model.generate(
-            input_ids      = input_gen.to(self.device),
-            attention_mask = attention_mask_gen.to(self.device),
-            pad_token_id   = self.tokenizer.pad_token_id,
-            )
-        
-        input_train = response_gen[:, 1:] # remove leading pad token
-        attention_mask_train = torch.ones_like(input_train)
-        labels = input_gen
-        labels[labels==self.tokenizer.pad_token_id] = -100
-
-        outputs = training_model(
-            input_ids      = input_train.to(self.device),
-            attention_mask = attention_mask_train.to(self.device),
-            labels         = labels.to(self.device)
-            )
-        
-        return outputs
-
-    def training_step(self, batch, batch_idx):
-        opt_a, opt_b = self.optimizers()
-        sch_a, sch_b = self.lr_schedulers()
-        
-        if batch['a']:
-            loss_a = self.cycle(batch['a'], self.model_b, self.model_a).loss
-            self.manual_backward(loss_a)
-
-            if batch_idx % self.gradient_accumulation_steps == 0:
-                opt_a.step()
-                sch_a.step()
-                opt_a.zero_grad()
-
-                self.log('train_loss_a', loss_a, on_step=True, logger=True)
-                self.log('opt_a_lr', opt_a.param_groups[0]['lr'], on_step=True, logger=True)
-
-        if batch['b']:
-            loss_b = self.cycle(batch['b'], self.model_a, self.model_b).loss
-            self.manual_backward(loss_b)
-
-            if batch_idx % self.gradient_accumulation_steps == 0:
-                opt_b.step()
-                sch_b.step()
-                opt_b.zero_grad()
-
-            self.log('train_loss_b', loss_b, on_step=True, logger=True)
-            self.log('opt_b_lr', opt_b.param_groups[0]['lr'], on_step=True, logger=True)
-
-    def validation_step(self, batch, batch_idx):
-        # TODO: validate whether this is the mean loss over the whole validation set or just the final batch eval loss
-        if batch['a']:
-            loss_a = self.model_a(**batch['a']).loss
-            self.log('val_loss_a', loss_a, on_epoch=True, logger=True)
-        if batch['b']:
-            loss_b = self.model_b(**batch['b']).loss
-            self.log('val_loss_b', loss_b, on_epoch=True, logger=True)
-
-    def train_dataloader(self):
-        loader_a = DataLoader(self.train_data_a, self.batch_size, shuffle=True, collate_fn=self.collator_a)
-        loader_b = DataLoader(self.train_data_b, self.batch_size, shuffle=True, collate_fn=self.collator_b)
-        return CombinedLoader({"a": loader_a, "b": loader_b}, 'max_size')
-
-    def val_dataloader(self):
-        loader_a = DataLoader(self.val_data_a, self.batch_size, shuffle=True, collate_fn=self.collator_a)
-        loader_b = DataLoader(self.val_data_b, self.batch_size, shuffle=True, collate_fn=self.collator_b)
-        return CombinedLoader({"a": loader_a, "b": loader_b}, 'max_size')
+from CycleModel import CycleModel
 
 def main(args):
     seed_everything(args.seed)
@@ -173,6 +16,7 @@ def main(args):
     )
 
     trainer = Trainer(
+        default_root_dir=args.output_dir + '/checkpoints',
         max_epochs=1,
         log_every_n_steps=1
     )
@@ -180,9 +24,13 @@ def main(args):
         trainer.logger = WandbLogger(project=args.wandb_project, entity=args.wandb_entity, name=args.wandb_run_name)
     trainer.fit(model)
 
+    # Save the models separately for use in inference
+    model.save_pretrained(args.output_dir)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name_or_path', type=str, default='gpt2', help='Model name or path to model for model A (default: gpt2)')
+    parser.add_argument('--output_dir', type=str, default='models', help='Path to save model checkpoints (default: models)')
     parser.add_argument('--data_a', type=str, default='data/example/A', help='Path to data for model A. If this a validation set can be found, it will be loaded (default: data/example/A))')
     parser.add_argument('--data_b', type=str, default='data/example/B', help='Path to data for model B. If this a validation set can be found, it will be loaded (default: data/example/B)))')
     parser.add_argument('--task', type=str, default='causal_lm', help='Task for model A (default: causal_lm))')
