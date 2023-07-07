Added data collator for causal lm and fixed numerous bugs with causal modelling

CycleModel.py
@@ -5,16 +5,29 @@ from pytorch_lightning import LightningModule
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM
 from transformers import DataCollatorForSeq2Seq
+from data_loader import DataCollatorForCausalLM
 import torch
 from data_loader import TrainDataset
 from lightning.pytorch.utilities import CombinedLoader
 
 class CycleModel(LightningModule):
-    def __init__(self, model_name_or_path, data_a, data_b, task, batch_size, gradient_accumulation_steps):
+    def __init__(self,
+                model_name_or_path: str,
+                task: str = 'CAUSAL_LM',
+                data_a: str = None,
+                data_b: str = None,
+                lr_a: float = 2e-4,
+                lr_b: float = 2e-4,
+                train_batch_size: int = 8,
+                eval_batch_size: int = 8,
+                gradient_accumulation_steps: int = 1,
+                **kwargs
+                ):
         super().__init__()
 
         self.task = task.upper()
 
+        self.save_hyperparameters()
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
 
         if not self.tokenizer.pad_token:
@@ -25,32 +38,29 @@ class CycleModel(LightningModule):
             self.model_b = AutoModelForCausalLM.from_pretrained(model_name_or_path)
             self.tokenizer.padding_side = 'left'
             self.cycle = self._cycle_causal
+            self.collator = DataCollatorForCausalLM(self.tokenizer, return_tensors='pt', padding=True)
         elif self.task == 'SEQ2SEQ_LM':
             self.model_a = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
             self.model_b = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
             self.cycle = self._cycle_seq2seq
+            self.collator = DataCollatorForSeq2Seq(self.tokenizer, return_tensors='pt', padding=True)
         else:
-            raise ValueError(f'Unknown task {self.task}')
+            raise ValueError(f'Unknown task {self.task}. Must be one of "causal_lm" or "seq2seq_lm"')
+            
 
-        self.collator_a = DataCollatorForSeq2Seq(self.tokenizer, return_tensors='pt', padding=True)
-        self.collator_b = DataCollatorForSeq2Seq(self.tokenizer, return_tensors='pt', padding=True)
-
-        self.train_data_a, self.val_data_a = TrainDataset(self.tokenizer).load_data(data_a)
-        self.train_data_b, self.val_data_b = TrainDataset(self.tokenizer).load_data(data_b)
-
-        self.batch_size = batch_size
-        self.gradient_accumulation_steps = gradient_accumulation_steps
+        self.train_data_a, self.val_data_a = TrainDataset(self.tokenizer, task=self.task).load_data(data_a)
+        self.train_data_b, self.val_data_b = TrainDataset(self.tokenizer, task=self.task).load_data(data_b)
 
         self.automatic_optimization = False
 
     def configure_optimizers(self):
         opt_a = Adam(
             self.model_a.parameters(),
-            lr=2e-4, betas=(0.5, 0.999))
+            lr=self.hparams.lr_a, betas=(0.5, 0.999))
         
         opt_b = Adam(
             self.model_b.parameters(),
-            lr=2e-4, betas=(0.5, 0.999))
+            lr=self.hparams.lr_b, betas=(0.5, 0.999))
         
         gamma = lambda epoch: 1 - max(0, epoch + 1 - 100) / 101
         sch_a = LambdaLR(opt_a, lr_lambda=gamma)
@@ -75,11 +85,11 @@ class CycleModel(LightningModule):
         labels = torch.cat((response_gen.to(self.device), prompt_gen), dim=1)
         labels[labels==self.tokenizer.pad_token_id] = -100
 
-        # pad new input to match label size
+        # pad new input to match label size        
         pad_size = labels.shape[-1] - response_gen.shape[-1]
         pad_tensor = torch.full((labels.shape[0], pad_size), self.tokenizer.pad_token_id)
         prompt_train = torch.cat((pad_tensor.to(self.device), response_gen), dim=-1)
-        attention_mask_train = torch.ones_like(prompt_train)
+        attention_mask_train = torch.cat((torch.zeros_like(pad_tensor.to(self.device)), torch.ones_like(response_gen)), dim=-1)
 
         outputs = training_model(
             input_ids      = prompt_train.to(self.device),
@@ -99,6 +109,7 @@ class CycleModel(LightningModule):
         
         input_train = response_gen[:, 1:] # remove leading pad token
         attention_mask_train = torch.ones_like(input_train)
+        attention_mask_train[input_train==self.tokenizer.pad_token_id] = 0 # Set padding tokens to 0
         labels = input_gen
         labels[labels==self.tokenizer.pad_token_id] = -100
 
@@ -118,47 +129,46 @@ class CycleModel(LightningModule):
             loss_a = self.cycle(batch['a'], self.model_b, self.model_a).loss
             self.manual_backward(loss_a)
 
-            if batch_idx % self.gradient_accumulation_steps == 0:
+            if batch_idx % self.hparams.gradient_accumulation_steps == 0:
                 opt_a.step()
                 sch_a.step()
                 opt_a.zero_grad()
 
-                self.log('train_loss_a', loss_a, on_step=True, logger=True)
+                self.log('train_loss_a', loss_a, on_step=True, logger=True, batch_size=self.hparams.train_batch_size)
                 self.log('opt_a_lr', opt_a.param_groups[0]['lr'], on_step=True, logger=True)
 
         if batch['b']:
             loss_b = self.cycle(batch['b'], self.model_a, self.model_b).loss
             self.manual_backward(loss_b)
 
-            if batch_idx % self.gradient_accumulation_steps == 0:
+            if batch_idx % self.hparams.gradient_accumulation_steps == 0:
                 opt_b.step()
                 sch_b.step()
                 opt_b.zero_grad()
 
-                self.log('train_loss_b', loss_b, on_step=True, logger=True)
+                self.log('train_loss_b', loss_b, on_step=True, logger=True, batch_size=self.hparams.train_batch_size)
                 self.log('opt_b_lr', opt_b.param_groups[0]['lr'], on_step=True, logger=True)
 
     def validation_step(self, batch, batch_idx):
-        # TODO: validate whether this is the mean loss over the whole validation set or just the final batch eval loss
         if batch['a']:
             loss_a = self.model_a(**batch['a']).loss
-            self.log('val_loss_a', loss_a, on_epoch=True, logger=True)
+            self.log('val_loss_a', loss_a, on_epoch=True, logger=True, batch_size=self.hparams.eval_batch_size)
         if batch['b']:
             loss_b = self.model_b(**batch['b']).loss
-            self.log('val_loss_b', loss_b, on_epoch=True, logger=True)
+            self.log('val_loss_b', loss_b, on_epoch=True, logger=True, batch_size=self.hparams.eval_batch_size)
 
     def train_dataloader(self):
-        loader_a = DataLoader(self.train_data_a, self.batch_size, shuffle=True, collate_fn=self.collator_a)
-        loader_b = DataLoader(self.train_data_b, self.batch_size, shuffle=True, collate_fn=self.collator_b)
+        loader_a = DataLoader(self.train_data_a, self.hparams.train_batch_size, shuffle=True, collate_fn=self.collator)
+        loader_b = DataLoader(self.train_data_b, self.hparams.train_batch_size, shuffle=True, collate_fn=self.collator)
         return CombinedLoader({"a": loader_a, "b": loader_b}, 'max_size')
 
     def val_dataloader(self):    
         if self.val_data_a:
-            loader_a = DataLoader(self.val_data_a, self.batch_size, shuffle=True, collate_fn=self.collator_a)
+            loader_a = DataLoader(self.val_data_a, self.hparams.eval_batch_size, shuffle=True, collate_fn=self.collator)
         else:
             loader_a = {}
         if self.val_data_b:
-            loader_b = DataLoader(self.val_data_b, self.batch_size, shuffle=True, collate_fn=self.collator_b)
+            loader_b = DataLoader(self.val_data_b, self.hparams.eval_batch_size, shuffle=True, collate_fn=self.collator)
         else:
             loader_b = {}
         return CombinedLoader({"a": loader_a, "b": loader_b}, 'max_size')

data_loader.py
@@ -1,11 +1,18 @@
 import os
 from datasets import load_from_disk, load_dataset
+from dataclasses import dataclass
+import numpy as np
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+from transformers.utils import PaddingStrategy
+from typing import Any, Callable, Dict, List, NewType, Optional, Tuple, Union
+
 
 class TrainDataset(object):
-    def __init__(self, tokenizer, max_length=512):
+    def __init__(self, tokenizer, task='CAUSAL_LM', max_length=512):
         super(TrainDataset, self).__init__()
 
         self.max_length = max_length
+        self.task = task.upper()
         self.tokenizer = tokenizer
 
     def tokenize(self, text):
@@ -17,6 +24,14 @@ class TrainDataset(object):
         )
 
         return result
+    
+    def build_labels(self, example):
+        if self.task == 'CAUSAL_LM':
+            return self.tokenize(example['text'] + ' ' + example['label'])['input_ids']
+        elif self.task == 'SEQ2SEQ_LM':
+            return self.tokenize(example['label'])['input_ids']
+        else:
+            raise ValueError(f'Unknown task {self.task}. Must be one of "causal_lm" or "seq2seq_lm"')
 
     def load_data(self, data_path):
         if data_path.endswith('.json') or data_path.endswith('.jsonl'):
@@ -27,12 +42,14 @@ class TrainDataset(object):
             load_dataset(data_path)
 
         data['train'] = data['train'].map(lambda x: self.tokenize(x['text']), remove_columns=['text'])
+        data['train'] = data['train'].filter(lambda x: len(x['input_ids']) > 0)
 
         if 'validation' in data.keys():
             data['validation'] = data['validation'].map(lambda x: {
                     **self.tokenize(x['text']),
-                    'labels': self.tokenize(x['label'])['input_ids']
+                    'labels': self.build_labels(x)
                 }, remove_columns=['text', 'label'])
+            data['validation'] = data['validation'].filter(lambda x: len(x['input_ids']) > 0)
         else:
             data['validation'] = None
 
@@ -70,3 +87,70 @@ class TestDataset(object):
         data['test'] = data['test'].map(lambda x: self.tokenize(x['text']), remove_columns=['text'])
 
         return data['test']
+    
+@dataclass
+class DataCollatorForCausalLM:
+
+    tokenizer: PreTrainedTokenizerBase
+    model: Optional[Any] = None
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    label_pad_token_id: int = -100
+    return_tensors: str = "pt"
+
+    def __call__(self, features, return_tensors=None):
+        if return_tensors is None:
+            return_tensors = self.return_tensors
+        labels = [feature["labels"] for feature in features] if "labels" in features[0].keys() else None
+
+        if labels is not None:
+            max_label_length = max(len(l) for l in labels)
+
+            if self.pad_to_multiple_of is not None:
+                max_label_length = (
+                    (max_label_length + self.pad_to_multiple_of - 1)
+                    // self.pad_to_multiple_of
+                    * self.pad_to_multiple_of
+                )
+
+            padding_side = self.tokenizer.padding_side
+            for feature in features:
+                remainder = [self.label_pad_token_id] * (max_label_length - len(feature["labels"]))
+                if isinstance(feature["labels"], list):
+                    feature["labels"] = (
+                        feature["labels"] + remainder if padding_side == "right" else remainder + feature["labels"]
+                    )
+                elif padding_side == "right":
+                    feature["labels"] = np.concatenate([feature["labels"], remainder]).astype(np.int64)
+                else:
+                    feature["labels"] = np.concatenate([remainder, feature["labels"]]).astype(np.int64)
+
+                remainder = [self.tokenizer.pad_token_id] * (max_label_length - len(feature["input_ids"]))
+                attn_remainder = [0] * (max_label_length - len(feature["attention_mask"]))
+                if isinstance(feature["input_ids"], list):
+                    feature["input_ids"] = (
+                        feature["input_ids"] + remainder if padding_side == "right" else remainder + feature["input_ids"]
+                    )
+                    feature["attention_mask"] = (
+                        feature["attention_mask"] + attn_remainder if padding_side == "right" else attn_remainder + feature["attention_mask"]
+                    )
+                elif padding_side == "right":
+                    feature["input_ids"] = np.concatenate([feature["input_ids"], remainder]).astype(np.int64)
+                    feature['attention_mask'] = np.concatenate([feature['attention_mask'], np.zeros_like(remainder)]).astype(np.int64)
+                else:
+                    feature["input_ids"] = np.concatenate([remainder, feature["input_ids"]]).astype(np.int64)
+                    feature['attention_mask'] = np.concatenate([np.zeros_like(remainder), feature['attention_mask']]).astype(np.int64)
+
+        features = self.tokenizer.pad(
+            features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=return_tensors,
+        )
+
+        return features
+
+
+