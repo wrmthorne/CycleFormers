38eb8a39847b5b28f50e2b7057e2193d9d5d4428
Liam <liamthorne4@gmail.com>
2024-02-24 01:15:33 +0000

    Successfully turned trainer_args into mixin to build model args class properly

---
+++ tests/trainer/test_training_args.py
@@ -0,0 +1,13 @@
+import unittest
+
+from transformers import TrainingArguments as HFTrainingArguments
+
+from cycleformers.trainer.trainer_args import ModelTrainingArguments, TrainingArguments
+
+
+class TestTrainingArguments(unittest.TestCase):
+    def test_training_arguments(self):
+        hf_args = HFTrainingArguments(output_dir='./test')
+        my_args = TrainingArguments(output_dir='./test')
+
+        self.assertDictEqual(hf_args.to_dict(), my_args.to_dict())
